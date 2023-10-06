29f34da7990fe2b28328aee66bc5c5c021b27761
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-10-06 01:53:23 -0300

    Poniendo todo junto
    Tarea Requerida - Poniendo Todo Junto

--- apoyalo/src/Componentes/PersonCard.jsx
+++ apoyalo/src/Componentes/PersonCard.jsx
@@ -1,13 +1,28 @@
 import { Component } from "react";
 import "./PersonCard.css"
 
-class PersonCard extends Component{
-    render (){
-        return(
+
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sumEdad: this.props.edad,
+        }
+    }
+    clickEdad = () => {
+        this.setState({
+            sumEdad: this.state.sumEdad + 1
+        })
+    }
+    render() {
+        let sumEdad = this.state.sumEdad;
+        return (
             <div>
                 <h1>{this.props.apellido}, {this.props.nombre}</h1>
-                <p>Age: {this.props.edad}</p>
+                <p>Age: {sumEdad}</p>
                 <p>Hair Color: {this.props.colorCabello}</p>
+                <button onClick={this.clickEdad}>Birthday Button for {this.props.nombre} {this.props.apellido}</button>
             </div>
         );
     }
